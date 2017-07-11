584639123168026c54d1927cd7164cb8818c1bf3
jwats287 <jwats287@users.noreply.github.com>
2017-07-10 21:46:08 -0400

    Read through and converted to jQuery

--- homework/unit_01/javaScript/stopwatch/hw.js
+++ homework/unit_01/javaScript/stopwatch/hw.js
@@ -18,50 +18,99 @@ const Stopwatch = {
   laps: [],
   // DO NOT EDIT ABOVE THIS LINE
   advanceTenMillisecs: function(){
-    // Your Code Here
+    this.millisecs += 10;
+    if (this.millisecs >= 1000) {
+      this.millisecs -= 1000;
+      this.secs++;
+    }
+    if (this.secs >= 60) {
+      this.secs -= 60;
+      this.mins++;
+    }
   },
   reset: function(){
-    // Your Code Here
+    this.mins = 0;
+    this.secs = 0;
+    this.millisecs = 0;
+    this.laps = [];
   },
   start: function(){
-    // Your Code Here
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.tickClock();
+    }
   },
   stop: function(){
-    // Your Code Here
+    this.isRunning = false;
   },
-  lap: function(){
-    // Your Code Here
+lap: function(){
+    if (this.isRunning) {
+      this.laps.push({
+        mins: this.mins,
+        secs: this.secs,
+        millisecs: this.millisecs
+      });
+    }
   }
 };
 
 /// User Interface ///
 const ViewEngine = {
   updateTimeDisplay: function(mins, secs, millisecs){
-    // Your Code Here
+    $('#mins').html(ViewHelpers.zeroFill(mins, 2));
+    $('#secs').html(ViewHelpers.zeroFill(secs, 2));
+    $('#millisecs').html(ViewHelpers.zeroFill(millisecs / 10, 2));
   },
-  updateLapListDisplay: function(laps){
-    // Your Code Here
+  updateLapListDisplay: function(){
+    var laps = Stopwatch.laps;
+    var lapList = $('#lap-list');
+    lapList.html('');
+    for (var i = 0; i < laps.length; i++) {
+      lapList.innerHTML += "\
+      <li>" +
+        ViewHelpers.zeroFill(laps[i].mins, 2) + ":" +
+        ViewHelpers.zeroFill(laps[i].secs, 2) + ":" +
+        ViewHelpers.zeroFill(laps[i].millisecs/10, 2) +
+      "</li>";
+    }
   },
 };
 const ViewHelpers = {
   zeroFill: function(number, length){
-    // Your Code Here
+    var str = number.toString();
+    let numZeroes = Math.max(length - str.length, 0);
+    for( var i = 0; i < (length - str.length); i++){
+      str = '0' + str;
+    }
+    return str;
   },
 };
 
 /// Top-Level Application Code ///
 const AppController = {
   handleClockTick: function(){
-    // Your Code Here
+    ViewEngine.updateTimeDisplay(Stopwatch.mins, Stopwatch.secs, Stopwatch.millisecs);
   },
   handleClickStart: function() {
-    console.log('Start the clock');
-  },
-  handleClickStopReset: function(){
-    console.log('Stop/reset the clock');
+    console.log("Start the clock");
+    if (!Stopwatch.isRunning) { Stopwatch.start(); }
   },
   handleClickLap: function(){
-    console.log('Lap the clock');
+    console.log("lap the clock");
+    if (Stopwatch.isRunning) {
+      Stopwatch.lap();
+      ViewEngine.updateLapListDisplay(Stopwatch.laps);
+    }
+  },
+  handleClickStopReset: function(){
+    console.log('reset the clock');
+    if (Stopwatch.isRunning) {
+      Stopwatch.stop();
+    } else {
+      Stopwatch.reset();
+      ViewEngine.updateTimeDisplay(0, 0, 0);
+      ViewEngine.updateLapListDisplay(Stopwatch.laps);
+    }
   }
 };
 
