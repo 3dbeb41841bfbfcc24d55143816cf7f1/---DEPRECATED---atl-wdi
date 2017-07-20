831d5d2d1ae2ba4a4becab6e65a114d72afd2bd5
jwats287 <jwats287@users.noreply.github.com>
2017-07-19 21:14:42 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.get('/', (req, res) => {
+    res.send(`Welcome to Pizza Express!`);
+})
 
+app.get('/topping/:topping', (req, res) => {
+    var topping = req.params.topping;
+    res.send(`${topping} pizza! Good choice!`);
+})
+
+app.get('/order/:number/:size', (req, res, next) => {
+    var number = req.params.number;
+    var size = req.params.size;
+    res.send(`Your order for ${number} ${size} pizzas will be ready in 1 minute!`);
+})
 
 app.listen(port, () => {
     console.log("=================================")
