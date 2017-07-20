90e3a0656fd0700133ee6cbe72ed09e700c8d3d8
jwats287 <jwats287@users.noreply.github.com>
2017-07-19 21:35:59 -0400

    refactored routes and added views

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -2,19 +2,31 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.set("view engine", "hbs");
+
 app.get('/', (req, res) => {
-    res.send(`Welcome to Pizza Express!`);
+    // res.send(`Welcome to Pizza Express!`);
+    res.render('index', {
+
+    })
 })
 
 app.get('/topping/:topping', (req, res) => {
     var topping = req.params.topping;
-    res.send(`${topping} pizza! Good choice!`);
+    // res.send(`${topping} pizza! Good choice!`);
+    res.render('toppings', {
+        data: topping
+    })
 })
 
 app.get('/order/:number/:size', (req, res, next) => {
     var number = req.params.number;
     var size = req.params.size;
-    res.send(`Your order for ${number} ${size} pizzas will be ready in 1 minute!`);
+    // res.send(`Your order for ${number} ${size} pizzas will be ready in 1 minute!`);
+    res.render('order', {
+        number: number,
+        size: size
+    })
 })
 
 app.listen(port, () => {
