49d2e28b4280bbe095b8655c74b575863933b436
jwats287 <jwats287@users.noreply.github.com>
2017-07-23 21:57:13 -0400

    Housekeeping

--- morning_exercises/unit_02/pseudocode/script.js
+++ morning_exercises/unit_02/pseudocode/script.js
@@ -8,22 +8,39 @@ var barrels = function(small, large, total){
 
 return largeBarrel;
 };
-
+barrels(2, 5, 825);
 //*************************
 // Problem 2:
 // Sailing the Seas
 
 var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+  const earthCircumference = 24901;
+
+
+  var gallonsNeeded = earthCircumference / milesPerGallon;
+  var totalCost = gallonsNeeded * fuelPrice;
+
+  return totalCost;
 };
+
+// shipFuelCost(3, 12) => 6225.25
 
 //*************************
 // Problem 3:
 // GROG
 
-var calcFruitJuice = function(a, b, c, d){
+var calcFruitJuice = function(gallonsA, percentageA, gallonsB, percentageB){
   //your code here
+  var totalGallons = gallonsA + gallonsB;
+    var totalCostcoJuice = gallonsA * percentageA;
+    var totalKirklandJuice = gallonsB * percentageB;
+  var totalJuice = totalCostcoJuice + totalKirklandJuice;
+
+  var answer = (totalJuice / totalGallons) / 100;
+  return answer;
 };
+
+// calcFruitJuice(3, 20, 2, 55) => 0.34
 
 //DO NOT EDIT BELOW THIS LINE//
 module.exports = {
