0a4d0c60acc47256796a06fd9b05631bd9feadc8
jwats287 <jwats287@users.noreply.github.com>
2017-07-26 21:40:10 -0400

    created donut schema

--- homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,21 @@
 //requirements: require mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
+mongoose.Promise = global.Promise;
 //create your donut schema:
-
+var donutSchema = new Schema({
+    name: String,
+    descriptio: String,
+    img: String,
+    price: Number,
+    qty: Number
+})
 
 //export your donut with module.exports()
+
+var DonutModel = mongoose.model("Donut", donutSchema);
+
+module.exports = {
+    Donut: DonutModel
+}
