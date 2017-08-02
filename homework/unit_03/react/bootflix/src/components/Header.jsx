2950259192727a6a45a1518922d90e381eba8360
king0120 <king0120@gmail.com>
2017-08-02 11:14:55 -0400

    Added bootflix HW

---
+++ homework/unit_03/react/bootflix/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// You don't need to do anything with this component
+const Header = () => {
+  return (
+    <header>
+        <hgroup>
+            <h2>Bootflix (alpha)</h2>
+            <h3>powered by <a href="http://www.omdbapi.com/" rel="noopener noreferrer" target="_blank">omdb</a></h3>
+        </hgroup>
+    </header>
+  );
+};
+
+export default Header;
