d8a34b56a1fcf22e0f0ad78fb4a78de4fdeccc92
ern19 <aarontrammell19@gmail.com>
2017-09-07 09:42:15 -0400

    finished morning exercise

--- morning_exercises/pseudocode/script.js
+++ morning_exercises/pseudocode/script.js
@@ -3,24 +3,41 @@
 // Barrels O' RUM
 
 var barrels = function(small, large, total){
+  var smallCost = small * 60;
+  var largeCost = total - smallCost;
+  totalBarrels = small + large;
+  largeRumPrice = largeCost / totalBarrels;
+  return "$" + parseInt(largeRumPrice);
+  
 };
+
+
+barrels(2,4,825);
 
 //*************************
 // Problem 2:
 // Sailing the Seas
 
 var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+  var distance = 24901;
+  totalGallons = fuelPrice / milesPerGallon;
+  totalFuelCost = totalGallons * distance;
+  return "$" + totalFuelCost;
 };
+
+shipFuelCost(3, 12);
 
 //*************************
 // Problem 3:
 // GROG
 
 var calcFruitJuice = function(a, b, c, d){
-  //your code here
+  var totalGallonsFruitJuice = (a * (b / 100)) + (c * (d / 100));
+  var totalGallons = a + c;
+  var percentFruitJuice = (totalGallonsFruitJuice / totalGallons) * 100;
+  return "%" + percentFruitJuice;
 };
-
+calcFruitJuice(3, 20, 2, 55);
 //DO NOT EDIT BELOW THIS LINE//
 module.exports = {
   barrels: barrels,
