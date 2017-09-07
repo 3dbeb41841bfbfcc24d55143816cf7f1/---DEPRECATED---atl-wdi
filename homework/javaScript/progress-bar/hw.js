7be30c511034aed8e6fdbf1c7184aa32a666f6b2
lcarswell6 <lcarswell6@gmail.com>
2017-09-07 16:57:38 -0400

    tracked changes

--- homework/javaScript/progress-bar/hw.js
+++ homework/javaScript/progress-bar/hw.js
@@ -4,15 +4,34 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+   document.getElementById('numeric-display').textContent = timerValue;
+
   },
   drawProgressBars: function(timerValue){
-    // Your Code Here
+    var progress = 100 - timerValue;
+    var bars = document.getElementsByClassName('progress-bar');
+    for (var i = 0; i < bars.length; i++) {
+      bars[i].style.width = progress + '%';
+    }
+
+
   },
   drawLitFuses: function(timerValue){
-    // Your Code Here
-  },
-  drawCrawlers: function(timerValue){
-    // Your Code Here
+    var percentUnburnt = timerValue/100;
+    var numFuses = document.getElementsByClassName('fuse').length;
+    for (var i = 0; i < numFuses; i ++) {
+    document.getElementsByClassName('unburnt')[i].style.width = percentUnburnt*98 + '%';
+    document.getElementsByClassName('burnt')[i].style.width = (1 - percentUnburnt)*98 + '%';
+    }
   }
-};
+},
+  drawCrawlers: function(timerValue){
+  var timeElapsed = 100 - timerValue;
+  if (timerValue%2 === 0) {
+    document.getElementsByClassName('crawler')[0].style.marginTop = 0px;
+  }
+  else {
+    document.getElementsByClassName('crawler')[0].style.marginTop = 10px;
+  }
+  document.getElementsByClassName('crawler')[0]style.marginLeft = (timeElapsed * 10) + px;
+},
--- labs/javaScript/independent-click-events/traffic_light/app.js
+++ labs/javaScript/independent-click-events/traffic_light/app.js
@@ -1,12 +1,15 @@
 window.onload = function() {
   var stopButton = document.querySelector('#stopButton');
   var slowButton = document.querySelector('#slowButton');
+  var goButton = document.querySelector('#goButton');
 
   var stopLight = document.querySelector('#stopLight');
   var slowLight = document.querySelector('#slowLight');
+  var goLight = document.querySelector('#goLight');
 
   stopButton.addEventListener('click', trafficLight.illuminateRed);
   slowButton.addEventListener('click', trafficLight.illuminateYellow);
+  goButton.addEventListener('click', trafficLight.illuminateGreen);
 }
 
 
@@ -15,14 +18,26 @@ var trafficLight = {
     trafficLight.clearLights();
 
     stopLight.style.background = 'red';
+    slowLight.style.background = 'black';
+    goLight.style.background = 'black';
   },
 
   illuminateYellow: function(event) {
     trafficLight.clearLights();
 
-    slowLight.style.background = 'blue';
+    slowLight.style.background = 'yellow';
+    stopLight.style.background = 'black';
+    goLight.style.background = 'black';
   },
 
+  illuminateGreen: function(event) {
+    trafficLight.clearLights();
+
+    goLight.style.background = 'green';
+    stopLight.style.background = 'black';
+    slowLight.style.background = 'black';
+  },
+  
   clearLights: function() {
     stopLight.style.background = 'black';
 
