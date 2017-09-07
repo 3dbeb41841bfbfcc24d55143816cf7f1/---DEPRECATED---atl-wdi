4a85a0aefc1abcaeacefad27c62c393d6c67ffe4
ern19 <aarontrammell19@gmail.com>
2017-09-06 20:34:42 -0400

    tried some stuff

--- homework/javaScript/progress-bar/hw.js
+++ homework/javaScript/progress-bar/hw.js
@@ -3,29 +3,29 @@
 // See LICENSE for details.
 
 var timerUI = {
-  drawNumericDisplay: function(timerValue){
-    document.getElementById("numeric-display").textContent = timerValue;
-      
-
-    }
+  drawNumericDisplay: function(timerValue){  
+    document.getElementById('numeric-display').textContent = timerValue;
   },
   drawProgressBars: function(timerValue){
+   
     var timeElapsed = 100 - timerValue;
-    document.getElementsByClassName("progress-bar")[0].style.width = timeElapsed + '%';
+    document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
   },
   drawLitFuses: function(timerValue){
-    var percentUnburnt = timerValue / 100;
-    document.getElementsByClassName('unburnt')[0].style.width = percentUnburnt*98 + "%";
-    document.getElementsByClassName('burnt')[0].style.width = (1 - percentUnburnt)*98 + "%";
-
+    
+    var percentUnburnt = timerValue/100;
+    document.getElementsByClassName('unburnt')[0].style.width = percentUnburnt*98 + '%';
+    document.getElementsByClassName('burnt')[0].style.width = (1 - percentUnburnt)*98 + '%';
+  },
   drawCrawlers: function(timerValue){
+    
     var timeElapsed = 100 - timerValue;
     if (timerValue%2 === 0) {
-      document.getElementsByClassName('crawler')[0].style.width = '0px';
-    } 
-    else {
-      document.getElementsByClassName('crawler')[0].style.width = '10px';
+      document.getElementsByClassName('crawler')[0].style.marginTop = '0px';
     }
-    document.getElementsByClassName('crawler')[0].style.width = '';
+    else {
+      document.getElementsByClassName('crawler')[0].style.marginTop = '10px';
+    }
+    document.getElementsByClassName('crawler')[0].style.marginLeft = (timeElapsed*10) + 'px';
   }
 };
