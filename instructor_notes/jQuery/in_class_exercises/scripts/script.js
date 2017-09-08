2cdd5ee4c575cf8dfac661ad3440b201b298ba58
king0120 <king0120@gmail.com>
2017-09-08 09:31:14 -0400

    added tasks for inclass exercise

--- instructor_notes/jQuery/in_class_exercises/scripts/script.js
+++ instructor_notes/jQuery/in_class_exercises/scripts/script.js
@@ -1,1 +1,11 @@
-console.log('hey')
+// 1. Use jQuery to fetch all `h1` tags and save them to a variable called $h1
+
+// 2. Grab the HTML element that is both an H1 and has an ID of "second"
+
+// 3. Select the div with an ID of 'myDiv' and change the text to say "jQuery makes JS easy!"
+
+// 4. Append this image to end of the body. http://www.fillmurray.com/200/200
+
+// 5. Change the background color of the page to green.
+
+// 6. Remove the div with and ID of 'myDiv'
