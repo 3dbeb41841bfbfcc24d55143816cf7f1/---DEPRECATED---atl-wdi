4f5678dcbf138760c3c2360fb57d4f2835482cef
ern19 <aarontrammell19@gmail.com>
2017-09-18 17:42:30 -0400

    successful pirate joke

---
+++ homework/node/intro-to-node/pirate_jokes/main.js
@@ -0,0 +1,6 @@
+var notifier = require('node-notifier');
+
+notifier.notify({
+    'title': 'Joke of the Day',
+    'message': 'Q: How much did the pirate pay for his piercings? \nA: A buck-an-ear.'
+});
