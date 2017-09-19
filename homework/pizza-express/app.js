f69c56f2ee160c448bb337b8f022ad5526c95c35
ern19 <aarontrammell19@gmail.com>
2017-09-19 17:42:13 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -2,7 +2,21 @@ var express = require('express');
 //save an express module as 'app'
 var app     = express();
 // assigning 3000 as our port
-var port    = 3000;
+app.get("/", (req, res) => {
+    res.send("Welcome to Pizza Express")
+})
+
+app.get("/topping/:type", (req, res, next) => {
+    const type = req.params.type;
+    res.send(`${type} pizza! Good choice.`)
+})
+
+app.get("/order/:amount/:size", (req, res, next) => {
+    const amount = req.params.amount
+    const size = req.params.size
+    res.send(`Your order for ${amount} ${size} pizzas will be ready in 1 minute`)
+})
+var port = 3000;
 
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
