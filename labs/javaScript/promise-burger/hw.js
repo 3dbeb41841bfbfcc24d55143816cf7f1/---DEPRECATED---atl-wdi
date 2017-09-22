5a2efe844e6aa9789703b867bd9941c6de31b832
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-09-22 16:38:07 -0400

    promise burger lab/homework

--- labs/javaScript/promise-burger/hw.js
+++ labs/javaScript/promise-burger/hw.js
@@ -7,8 +7,7 @@ const kitchen = require('./kitchen');
 // Call this method and console.log the data that comes back when the Promise is resolved.
 
 //CODE FOR QUESTION 1 HERE
-
-
+kitchen.readMenu().then(console.log).catch(console.log);
 // Order Some Food
 /* *************************************************************** */
 // 2. For this task, you will use the method `.order` to order a a burger from the menu.  
@@ -16,14 +15,14 @@ const kitchen = require('./kitchen');
 //  Console.log the data that comes back about this burger.
 
 // CODE FOR QUESTION 2 HERE
-
+kitchen.order("rebel outlaw").then(console.log).catch(console.log);
 
 // 3. Try and use the same method to order a `quesadilla`.  
 //  Since Promise Burger only serves burgers, this will throw an error.  
 //  Make sure that your Promise can be dealt with when it is rejected.
 
 // CODE FOR QUESTION 3 HERE
-
+kitchen.order('quesadilla').then(console.log).catch(console.log);
 
 // Add A Burger To The Menu
 /* *************************************************************** */
@@ -41,13 +40,13 @@ const newBurger = {
 }
 
 // CODE FOR QUESTION 4 HERE
-
+kitchen.addToMenu(newBurger).then(console.log).catch(console.log);
 // 5. Validate that the new item has been added to the menu by calling `readMenu` again.  
 // We only want to call `readMenu` after we get a response that is successful. 
 // Remember that we can chain promises together the same way that we can chain jQuery methods.
 
 // REFACTOR QUESTION 4 TO ACCOUNT FOR THIS REQUIREMENT
-
+kitchen.readMenu().then(console.log).catch(console.log);
 // Stretch Goals
 /* *************************************************************** */
 // 6. Let's be honest, people only go to Promise Burger when they are showing out-of-towners around.  
