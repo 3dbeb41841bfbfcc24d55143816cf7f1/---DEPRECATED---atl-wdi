06411dd24b736b745c60d5b24338090ad33eda22
ern19 <aarontrammell19@gmail.com>
2017-10-11 08:01:09 -0400

    trying to fill in what I think should be there and work out bugs later

--- homework/react/bootflix/src/components/Movie.jsx
+++ homework/react/bootflix/src/components/Movie.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 // Update this Movie component with info from OMDB
 // BONUS: Use OMDB's Poster API to add a poster to each movie.
-const Movie = () => {
+const Movie = ({ title, year, director, genre, plot }) => {
   return (
     <section id="movie-listing">
       <div className="movie">
-        <h3>Movie Title</h3>
+        <h3>{title}</h3>
         <p>
-          <strong>Released:</strong> 1984<br />
-          <strong>Directed By:</strong> Some director<br />
-          <em>Genre:</em>
+          <strong>Released:</strong> {year}<br />
+          <strong>Directed By:</strong> {director}<br />
+          <em>Genre:{genre}</em>
         </p>
-        <p>Plotline should go inside of here</p>
+        <p>{plot}</p>
       </div>
     </section>
   );
--- homework/react/bootflix/src/components/Search.jsx
+++ homework/react/bootflix/src/components/Search.jsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // Update the forms to utilize methods being passed down from App.js
-const Search = () => {
-  return (
-    <section id="movie-search">
-      <strong>Search by:</strong> Title <em>or</em> ID
+class Search extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      value: ""
 
-      <div className="search">
-        <form id="title-search-form" method="get">
-          <input type="text" name="title" placeholder="Enter movie title" />
-          <input onSubmit={(event) => this.props._searchByTitle}type="submit" value="Search for this title" />
-        </form>
-      </div>
+    }
+  }
+  handleIdSubmit = (event) => {
+    event.preventDefault()
+    this.props._searchById(this.state.value)
 
-      <div className="search">
-        <form id="id-search-form" method="get">
-          <input type="text" name="id" placeholder="Enter omdb movie ID" />
-          <input onSubmit={(event) => this.props._searchById} type="submit" value="Search by ID" />
-        </form>
-      </div>
+    const newSearch = {
+      value: ""
+    }
+    this.setState({value: newSearch})
+  }
+  
+  handleTitleSubmit = (event) => {
+    event.preventDefault()
+    this.props._searchByTitle(this.state.value)
 
-    </section>
-  );
-};
+    const newSearch = {
+      value: ""
+    }
+    this.setState({value: newSearch})
+  }
 
+  handleIdChange =(event)=> {
+    const newSearch = {...this.state.value}
+    newSearch[event.target.name] = event.target.value
+    this.setState({value: newSearch})
+  }
+  render(){
+    return (
+      <section id="movie-search">
+        <strong>Search by:</strong> Title <em>or</em> ID
+
+        <div className="search">
+          <form id="title-search-form" method="get" onSubmit={this.handleTitleSubmit}>
+            <input type="text" name="title" placeholder="Enter movie title" />
+            <input onChange={this.handleTitleChange} type="submit" value={this.state.value} />
+          </form>
+        </div>
+
+        <div className="search">
+          <form id="id-search-form" method="get" onSubmit={this.handleIdSubmit}>
+            <input type="text" name="id" placeholder="Enter omdb movie ID" />
+            <input onChange={this.handleIdChange} type="submit" value={this.state.value} />
+          </form>
+        </div>
+
+      </section>
+    );
+  };
+}
 export default Search;
