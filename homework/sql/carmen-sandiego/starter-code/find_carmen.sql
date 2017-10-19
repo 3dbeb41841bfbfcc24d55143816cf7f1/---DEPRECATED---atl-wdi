5ceef18765818573039c59566729e49d5374ea12
ern19 <aarontrammell19@gmail.com>
2017-10-19 16:59:57 -0400

    halfway

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,8 @@
+1. The Vatican
+2.  SELECT * FROM countrylanguage WHERE countrycode='VAT'
+"Italian"
+3.  SELECT * FROM country WHERE code = 'SMR'; 
+"San-Marino"
+4. SELECT * FROM city where countrycode='SMR'
+"Serravalle"
+5.
