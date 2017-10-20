34ef87d4f4a053de9c26ede6eed4797bdbaf97a9
ern19 <aarontrammell19@gmail.com>
2017-10-19 20:24:52 -0400

    queried some nfl stuff

---
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -0,0 +1,5 @@
+1. SELECT name FROM teams
+2.SELECT stadium, head_coach FROM teams WHERE conference='NFC'
+3. SELECT head_coach FROM teams WHERE division='South' AND conference='AFC'
+4. SELECT COUNT(*) FROM players;
+5.
