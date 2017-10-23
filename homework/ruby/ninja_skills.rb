bdc3380ad479353b58fd1e3db31297aa450e5dd5
ern19 <aarontrammell19@gmail.com>
2017-10-23 17:42:35 -0400

    did some stuff

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -5,10 +5,55 @@
 # puts a != b
 
 #Nil
-p = nil 
-puts p
-puts %{p} " was here"
-puts p.class
+# p = nil 
+# puts p
+ puts "#{p}  was here"
+# puts p.class
+
+#Ints
+# d = 4
+# puts d
+# e = 2.0
+# puts e
+# d = e 
+# puts d
+
+#Strings
+# christine = "Hi, I'm Christine"
+# likesTo = "Long walks on the beach, kinda."
+# puts "#{christine}  and I like  #{likesTo}"
+# g = "2"
+# gToNumber = 2 
+
+# puts gToNumber + g.to_i
+
+#Input/Output
+
+# puts "Give me a number"
+# input = gets.chomp.to_i * 2
+# puts "Entry times 2 equals: #{input}"
+
+# puts "What yo name is"
+# name = gets.chomp
+# puts "What yo job is"
+# what_do = gets.chomp
+# puts "#{name} is a #{what_do}."
+
+#Arrays 
+myFirstArray = []
+myFirstArray = ["a", "b", "c", "d", "e", 1, 2, 3, 4]
+puts myFirstArray.last
+puts myFirstArray.first 
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray.push(true)
+myFirstArray.push(myFirstArray[3].class)
+puts myFirstArray
+
+
+
+
+
 
 
 
