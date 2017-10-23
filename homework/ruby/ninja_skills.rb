48992c22ef62be88a4c8c044f1e57172784cd1ac
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-10-23 17:42:10 -0400

    Ruby Ninja Integer skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -16,5 +16,19 @@ ${a} + was here
 => nil
 
 Challenge 3
+pry(main)> d = 4
+=> 4
+pry(main)> puts d
+4
 
+pry(main)> e = 2.3
+=> 2.3
+pry(main)> puts e
+2.3
 
+pry(main)> d = e
+=> 2.3
+pry(main)> puts d
+2.3
+
+Challenge 4
