7a0a14ec9673e6086b6c7562cfad22b2dc83b73e
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-10-23 17:35:55 -0400

    Ruby Ninja Nil skills

---
+++ homework/ruby/ninja_skills.rb
@@ -0,0 +1,20 @@
+Challenge 1
+[1] pry(main)> a = true
+=> true
+[2] pry(main)> b = false
+=> false
+[3] pry(main)> a = b
+=> false
+[4] pry(main)> a != b
+=> true
+
+Challenge 2
+pry(main)> a = nil
+=> nil
+pry(main)> puts "${a} + was here"
+${a} + was here
+=> nil
+
+Challenge 3
+
+
