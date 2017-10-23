ed675213e48485a11ad8e503b01da0b6a2e6edb2
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-10-23 13:45:35 -0400

    function drills hw complete

--- homework/javaScript/function-drills/hw.js
+++ homework/javaScript/function-drills/hw.js
@@ -11,7 +11,11 @@
 // Output: the sum of the numbers that were passed in
 // Edge Case: If the array is empty, return 0
 var sumOfNums = function(numsArray){
-  // Your Code Here
+ var sum = 0 ;
+ for (var i=0; i < numsArray.length, i++) {
+  sum += numsArray[i];
+ }
+ return sum;
 };
 
 // #2
@@ -19,7 +23,12 @@ var sumOfNums = function(numsArray){
 // Output: an array of the numbers from the first array that are strictly
 //         greater (i.e. greater than but not equal to) than 10
 var numsGreaterThanTen = function(numsArray){
-  // Your Code Here
+  var results  = [];
+  for (var i = 0; i < numsArray.length, i++) {
+    if (numsArray[i] > 10) {
+      results.push(numsArray[i]);
+    }
+  }
 };
 
 // #3
@@ -28,14 +37,24 @@ var numsGreaterThanTen = function(numsArray){
 //         `false` otherwise
 // Edge Case: If the input array is empty, the function should return `true`.
 var allGreaterThanTen = function(numsArray){
-  // Your Code Here
+  for (var i = 0; i < numsArray.length; i++) {
+    if (!(numsArray[i] > 10)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // #4
 // Input: an array of words
 // Output: an array of all words from the first array with five or more letters
 var wordsWithAtLeastFiveLetters = function(words){
-  // Your Code Here
+  var results = [];
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length >= 5) {
+      results.push(words[i]);
+    }
+  }
 };
 
 // #5
@@ -44,7 +63,12 @@ var wordsWithAtLeastFiveLetters = function(words){
 //          `false` otherwise
 // Edge Case: If the array is empty, the function should return `true`.
 var allStartingWithA = function(words){
-  // Your Code Here
+  for (var i = 0; i < words.length; i++) {
+    if (!(words[i].slice(0, 1) === 'a' || words[i].slice(0, 1) === 'A')) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // #6
@@ -52,7 +76,11 @@ var allStartingWithA = function(words){
 // Output: `true` if there are ANY words that start with the letter 'b'
 //          (case-insensitive), `false` otherwise
 var anyStartingWithB = function(words){
-  // Your Code Here
+  for (var i = 0; i < words.length, i++) {
+    if (words[i].slice(0, 1) === 'b' || words[i].slice(0, 1) === 'B') {
+    return true;
+    }
+  }return false;
 };
 
 // #7
@@ -62,7 +90,22 @@ var anyStartingWithB = function(words){
 //    Assume that vowels are 'a', 'e', 'i', 'o', and 'u' (NOT 'y')
 // Edge Case: If `n` is less than zero, return `null`.
 var hasAtLeastNVowels = function(word, n){
-  // Your Code Here
+if (n < 0) {
+  return null;
+}
+
+var count = 0;
+for (i =0; i <word.length; i++) {
+  if (word.charAt(i) === 'a' || word.charAt(i) === 'A' ||
+    word.charAt(i) === 'e' || word.charAt(i) === 'E' ||
+    word.charAt(i) === 'o' || word.charAt(i) === 'O' ||
+    word.charAt(i) === 'i' || word.charAt(i) === 'I' ||
+    word.charAt(i) === 'u' || word.charAt(i) === 'U'
+    ) { 
+      count++;
+      }
+  }
+return count >= n;
 };
 
 // #8
@@ -70,7 +113,13 @@ var hasAtLeastNVowels = function(word, n){
 // Output: an array of words from the original array that have at least two
 //          vowels
 var wordsWithAtLeastTwoVowels = function(words){
-  // Your Code Here
+  var results = [];
+  for (var i = 0; i < words.length; i ++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      results.push(words[i]);
+    }  
+  }
+  return results;
 };
 
 // #9
@@ -78,7 +127,12 @@ var wordsWithAtLeastTwoVowels = function(words){
 // Output: `true` if ALL words have two or more vowels, `false` otherwise
 // Edge Case: If the array is empty, the function should return `true`.
 var allHaveAtLeastTwoVowels = function(words){
-  // Your Code Here
+  for (var i = 0; i < words.length; i++) {
+    if (!hasAtLeastNVowels(words[i], 2)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // #10
@@ -86,7 +140,12 @@ var allHaveAtLeastTwoVowels = function(words){
 // Output: `true` if there are ANY words have two or more vowels,
 //          `false` otherwise.
 var anyHaveAtLeastTwoVowels = function(words){
-  // Your Code Here
+  for (var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // #11
@@ -95,7 +154,12 @@ var anyHaveAtLeastTwoVowels = function(words){
 //          `false` otherwise
 // Edge Case: If the array is empty, the function should return `true`.
 var noneHaveTwoOrMoreVowels = function(words){
-  // Your Code Here
+  for ( var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // #12
@@ -105,7 +169,11 @@ var noneHaveTwoOrMoreVowels = function(words){
 // e.g. given ['cat', 'horse', 'elephant'],
 //      return { cat: 3, horse: 5, elephant: 8}
 var buildObjectFromWords = function(words){
-  // Your Code Here
+  var result = {};
+  for (var i = 0; i < words.legth; i++) {
+    result[words[i]] = words[i].length;
+  }
+  return result;
 };
 
 
--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -2,8 +2,10 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+library=# SELECT * FROM authors JOIN books ON authors.id = books.author_id WHERE name LIKE '%Martin%';
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+library=# SELECT * FROM authors JOIN books ON authors.id = books.author_id WHERE name='Milan Kundera';
 
 -- Find all books written by an author from China or the UK.
 
@@ -14,9 +16,14 @@
 -- Find all books written after 1930 by authors from Argentina.
 
 -- Find all books written before 1980 by authors not from the US.
-
+SELECT * FROM books
+JOIN authors ON books.author_id = authors.id
+WHERE books.pubication_date < 1980
+AND authors.nationality != 'United States of America';
 -- Find all authors whose names start with 'J'.
-
+SELECT * FROM authors 
+WHERE name ILIKE 'j%';
 -- Find all books whose titles contain 'the'.
-
+SELECT * FROM books 
+WHERE title ILIKE '%the&';
 -- Find all authors who have written books with that start with the letter 'N'.
