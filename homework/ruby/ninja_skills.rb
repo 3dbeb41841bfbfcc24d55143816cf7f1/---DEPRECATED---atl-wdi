4461b211e69c4a1209590c9bb0accbb4ed4744d2
Leon Carswell <aleon@leons-air.attlocal.net>
2017-10-23 23:38:57 -0400

    ninja skills integers

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,3 +1,5 @@
+# challenge 1
+
 a = true
  puts a
 b = false 
@@ -5,3 +7,17 @@ puts b
 puts a = b
 puts a =! b
 
+# challenge 2
+
+# uno = nil 
+# puts uno + "nil was here"
+
+# challenge 3
+
+d = 7
+puts d
+e = 3.4
+puts e
+
+d = e 
+puts d
